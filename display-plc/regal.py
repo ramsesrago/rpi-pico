112c78105d56b2d41655a583d6a81166d6a4ea33
Ramses Ramirez <ramsesrago@gmail.com>
2023-09-13 21:39:37 -0600

    Adaptaciones de acuerdo a los requerimientos del prototipo

--- display-plc/regal.py
+++ display-plc/regal.py
@@ -19,8 +19,9 @@ BLUE = graphics.create_pen(8, 51, 162)
 # Timer definitions
 total_time = 100
 #est_time = time.localtime([2023,1,1,1,1,5,1,1])
-est_time = time.localtime(65)
+est_time = time.localtime(15)
 est_formatted_time = "{a:02d}:{b:02d}".format(a = est_time[4], b = est_time[5]%60)
+elapsed_time = 0
 
 # PLC definitions
 ## A0
@@ -40,6 +41,13 @@ def setup_display():
     graphics.clear()
     graphics.set_font("bitmap8")
     graphics.set_thickness(25)
+    graphics.set_pen(BLACK)
+    graphics.clear()
+    display_actual_time("00:00")
+    display_est_time()
+    display_curr_count()
+    display_delayed_pz()
+    i75.update(graphics)
     
 def display_actual_time(formatted_time):
     graphics.set_pen(GREEN)
@@ -64,9 +72,15 @@ def display_delayed_pz():
     graphics.text(delayed_pz_str, 32, 16, scale=2)
 
 def time_tick(timer):
+    print("time-tick")
     global start_time, total_time, elapsed_time
     current_time = time.time() # using the time() function will likely be more accurate than counting the ticks, plus we can mess with the tick frequency now.
     elapsed_time = current_time - start_time
+    
+    
+def update_display_callback(timer):
+    print("update callback")
+    global formatted_time, elapsed_time
     formatted_time = "{a:02d}:{b:02d}".format(a = elapsed_time//60, b = elapsed_time%60)
     graphics.set_pen(BLACK)
     graphics.clear()
@@ -77,17 +91,24 @@ def time_tick(timer):
     i75.update(graphics)
     
 def start_timer():
-    Timer().init(freq=1, mode=Timer.PERIODIC, callback=time_tick)
+    global tick_timer, update_display_timer
+    tick_timer = Timer(period=1000, mode=Timer.PERIODIC, callback=time_tick)
+    update_display_timer = Timer(period=500, mode=Timer.PERIODIC, callback=update_display_callback)
 
 # INICIO empieza timer
 def GPIO_A0_callback(pin):
     pin.irq(handler=None)
     print("GPIO_A0_callback Falling edge detected for: ", pin)
     # Start timer
-    global start_time
+    global start_time, GPIO_A1, GPIO_A2, elapsed_time, formatted_time
+    elapsed_time = 0
+    formatted_time = "00:00"
     start_time = time.time()
     start_timer()
     pin.irq(trigger=Pin.IRQ_FALLING, handler=GPIO_A0_callback)
+    # Despues del inicio de ciclo el micro esta listo para recibir otros eventos
+    GPIO_A1.irq(trigger=Pin.IRQ_FALLING, handler=GPIO_A1_callback)
+    GPIO_A2.irq(trigger=Pin.IRQ_FALLING, handler=GPIO_A2_callback)
 
 # CUENTA pieza y resetea T.Act
 
@@ -105,13 +126,15 @@ def GPIO_A0_callback(pin):
 def GPIO_A1_callback(pin):
     pin.irq(handler=None)
     print("GPIO_A1_callback Falling edge detected for: ", pin)
-    global on_time_pz, elapsed_time, delayed_pz
+    global on_time_pz, elapsed_time, delayed_pz, tick_timer
+    # Detiene el timer
+    tick_timer.deinit()
+    #elapsed_time = 0
     
     tiempo_total = est_time[5] + est_time[4]*60
     if (elapsed_time < tiempo_total):
         print("GPIO_A1_callback elapsed_time < est_time")
         on_time_pz += 1
-        # Pausar timepo
     else:
         print("GPIO_A1_callback elapsed_time >= est_time")
         on_time_pz += 1
@@ -127,17 +150,20 @@ def GPIO_A2_callback(pin):
     print("GPIO_A2_callback Falling edge detected for: ", pin)
     on_time_pz = 0
     delayed_pz = 0
+    tick_timer.deinit()
     pin.irq(trigger=Pin.IRQ_FALLING, handler=GPIO_A2_callback)
 
 # Can an interrupt be interrupted?
 def setup_gpios():
+    global GPIO_A0, GPIO_A1, GPIO_A2
     GPIO_A0 = Pin(26, Pin.IN, Pin.PULL_UP)
     GPIO_A1 = Pin(27, Pin.IN, Pin.PULL_UP)
     GPIO_A2 = Pin(28, Pin.IN, Pin.PULL_UP) 
     #GPIO_A0.irq(lambda pin: print("IRQ with flags:", pin.irq().flags()), Pin.IRQ_FALLING)
+    # Inicialmente solo se requiere la interrupcion de INICIO de ciclo
     GPIO_A0.irq(trigger=Pin.IRQ_FALLING, handler=GPIO_A0_callback)
-    GPIO_A1.irq(trigger=Pin.IRQ_FALLING, handler=GPIO_A1_callback)
-    GPIO_A2.irq(trigger=Pin.IRQ_FALLING, handler=GPIO_A2_callback)
+    #GPIO_A1.irq(trigger=Pin.IRQ_FALLING, handler=GPIO_A1_callback)
+    #GPIO_A2.irq(trigger=Pin.IRQ_FALLING, handler=GPIO_A2_callback)
 
 setup_display()
 setup_gpios()
